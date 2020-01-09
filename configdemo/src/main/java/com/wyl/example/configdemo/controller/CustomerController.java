da2f80acb672de2431973444e0f61215bd4c2770
wyl <wyl@example.com>
2020-01-09 17:35:19 +0800

    nacos 多命名空间配置

--- configdemo/src/main/java/com/wyl/example/configdemo/controller/CustomerController.java
+++ configdemo/src/main/java/com/wyl/example/configdemo/controller/CustomerController.java
@@ -1,26 +1,20 @@
 package com.wyl.example.configdemo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/config")
-@RefreshScope
-public class ConfigController {
-    @Value("${user.age:22}")
-    private String age;
-    @Value("${server.port:8080}")
-    private Integer port;
+@RequestMapping("/customer")
+public class CustomerController {
 
-    @RequestMapping("/get")
-    public String get() {
-        return age;
-    }
+    @Value("${server.port:1234}")
+    private Integer port;
 
     @RequestMapping("/getPort")
     public Integer getPort() {
         return port;
     }
+
+
 }
