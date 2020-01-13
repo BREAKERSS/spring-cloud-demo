1108f556ca5caf94f52884ec1c6797194d6434d9
wyl <wyl@example.com>
2020-01-13 18:49:45 +0800

    修改启动类注解

--- feigndemo-consumer/src/main/java/com/wyl/example/feigndemoconsumer/FeigndemoConsumerApplication.java
+++ feigndemo-consumer/src/main/java/com/wyl/example/feigndemoconsumer/FeigndemoConsumerApplication.java
@@ -1,33 +1,22 @@
 package com.wyl.example.feigndemoconsumer;
 
-import com.wyl.example.feigndemoprovider.service.testdemo.TestDemoFallBack;
-import com.wyl.example.feigndemoprovider.service.testdemo.TestDemoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
+/**
+ * @author wyl
+ */
 @SpringBootApplication
 @EnableFeignClients(basePackages = {"com.wyl.example"})
 //@MapperScan(basePackages = {"com.jiayiedu.mapper"})
 @ComponentScan(basePackages = {"com.wyl.example"})
-public class FeigndemoConsumerApplication implements CommandLineRunner {
+public class FeigndemoConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FeigndemoConsumerApplication.class, args);
     }
 
 
-    @Autowired
-    TestDemoService testDemoService;
-    @Autowired
-    TestDemoFallBack testDemoFallBack;
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(testDemoService.get());
-        System.out.println(testDemoFallBack.get());
-    }
 }
